6175116369c37ec388ddafcee7a33a3c515f6df5
j <jlwelsh@gmail.com>
2018-06-29 21:28:13 +0000

    corrected syntax errors

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -7,12 +7,12 @@
 #
 if node['platform_family'] == "rhel"
 	package = "httpd"
-else node['platform_family'] == "debian"
+elsif node['platform_family'] == "debian"
 	package = "apache2"
 end
 
 package 'apache2' do
-	package_name 'package'
+	package_name package
 	action :install
 end
 
